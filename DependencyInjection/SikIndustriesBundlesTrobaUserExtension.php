7bab85cee8e75719631d03876ed9e24195e1f58e
siklol <mail@veitosiander.de>
2014-01-17 22:51:26 +0100

    fixed name

--- DependencyInjection/SikIndustriesBundlesTrobaUserExtension.php
+++ DependencyInjection/SikIndustriesBundlesTrobaUserExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SikIndustriesBundlesUserExtension extends Extension
+class SikIndustriesBundlesTrobaUserExtension extends Extension
 {
     /**
      * {@inheritDoc}
