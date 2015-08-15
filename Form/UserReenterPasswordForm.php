d5d572dc9d05d79396bcbc046e43564e0427d873
Veit Osiander <mail@veitosiander.de>
2015-08-15 23:43:36 +0200

    fixed password reset

---
+++ Form/UserReenterPasswordForm.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siklol
+ * Date: 1/12/14
+ * Time: 4:28 PM
+ */
+
+namespace SikIndustries\Bundles\TrobaUserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserReenterPasswordForm extends AbstractType
+{
+    const NAME = 'user_reset_form';
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'The password fields must match.',
+                'options' => array('attr' => array('class' => 'password-field')),
+                'required' => true,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('save', 'submit')
+        ;
+    }
+
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
